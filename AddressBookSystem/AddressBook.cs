7d55bfbbe8a62a5777febfa966bc68ffed2db688
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-12 10:16:02 +0530

    feat:Welcome to Address Book System

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        private List<Contact> pcontacts { get; set; } = new List<Contact>();
+        public void AddContact(Contact contact)
+        {
+            pcontacts.Add(contact);
+        }
+
+        public void DisplayAllContact()
+        {
+            foreach (var contact in pcontacts)
+            {
+                Console.WriteLine("SAVED CONTACT IS :\n" + contact.FirstName + contact.LastName + "\nAddress: " + contact.Address + "\nCity: " + contact.City + "\nState: " + contact.State + "\nZip Code: " + contact.ZipCode + "\nPhone Number: " + contact.PhoneNumber + "\nEmail: " + contact.Email);
+               
+            }
+        }
+    }
+}
