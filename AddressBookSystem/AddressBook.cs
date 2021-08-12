d94155e2c769480fc55a8dd847ab8abf23ed36d3
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-12 10:47:14 +0530

    feat:Ability To Edit Contacts

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -17,8 +17,41 @@ namespace AddressBookSystem
             foreach (var contact in pcontacts)
             {
                 Console.WriteLine("SAVED CONTACT IS :\n" + contact.FirstName + contact.LastName + "\nAddress: " + contact.Address + "\nCity: " + contact.City + "\nState: " + contact.State + "\nZip Code: " + contact.ZipCode + "\nPhone Number: " + contact.PhoneNumber + "\nEmail: " + contact.Email);
-               
+            }
+            }
+            public void EditContact(string Name)
+            {
+                foreach (var contact in pcontacts)
+                {
+                    if (contact.FirstName == Name)
+                    {
+                        Console.WriteLine("Enter new First Name");
+                        string newFirstName = Console.ReadLine();
+                        contact.FirstName = newFirstName;
+                        Console.WriteLine("Enter new Last Name:");
+                        string newLastName = Console.ReadLine();
+                        contact.LastName = newLastName;
+                        Console.WriteLine("Enter new Address:");
+                        string newAddress = Console.ReadLine();
+                        contact.Address = newAddress;
+                        Console.WriteLine("Enter new City:");
+                        string newCity = Console.ReadLine();
+                        contact.City = newCity;
+                        Console.WriteLine("Enter new State:");
+                        string newState = Console.ReadLine();
+                        contact.State = newState;
+                        Console.WriteLine("Enter new Zip Code:");
+                        string newZipcode = Console.ReadLine();
+                        contact.ZipCode = newZipcode;
+                        Console.WriteLine("Enter new Phone Number:");
+                        string newPhonenumber = Console.ReadLine();
+                        contact.PhoneNumber = newPhonenumber;
+                        Console.WriteLine("Enter new Email:");
+                        string newEmail = Console.ReadLine();
+                        contact.Email = newEmail;
+                        Console.WriteLine("--------------------------------------------");
+                    }
+                }
             }
         }
     }
-}
