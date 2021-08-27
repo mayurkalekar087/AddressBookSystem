7520457037d2be392b1e5914253a703bc17202f9
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-27 23:11:52 +0530

    feat:Ability To Read Or Write Using FileIO

---
+++ AddressBookSystem/FileIO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace AddressBookSystem
+{
+    class FileIO
+    {
+        public static void WriteDataToTxt(Dictionary<string, Contact> DictName)
+        {
+            const string PATH = @"C:\Users\ASUS\source\repos\AddressBookSystem\AddressBookSystem\StoredContact.txt";
+            if (File.Exists(PATH))
+            {
+                using (TextWriter tw = new StreamWriter(PATH))
+                {
+                    tw.WriteLine(string.Format("FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,Email"));
+                    foreach (var element in DictName)
+                    {
+                        tw.WriteLine(string.Format($"{element.Value.FirstName},{element.Value.LastName},{element.Value.Address},{element.Value.City},{element.Value.State},{element.Value.ZipCode},{element.Value.PhoneNumber},{element.Value.Email}"));
+                    }
+                }
+            }
+            else Console.WriteLine("File doesn't exist, Check the Path");
+        }
+    }
+}
+
